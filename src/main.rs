48acb552ec73631517bbfc5f48e88ab5371ac599
Samyak S Sarnayak <samyak201@gmail.com>
2025-06-28 17:25:45 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,101 @@
-//! Very basic example to showcase how to use iroh's APIs.
-//!
-//! This example implements a simple protocol that echos any data sent to it in the first stream.
-//!
-//! ## Usage
-//!
-//!     cargo run --example echo --features=examples
+use std::path::PathBuf;
 
-use iroh::{
-    Endpoint, NodeAddr,
-    endpoint::Connection,
-    protocol::{AcceptError, ProtocolHandler, Router},
+use iroh::{Endpoint, protocol::Router};
+use iroh_blobs::{
+    net_protocol::Blobs, rpc::client::blobs::WrapOption, store::{ExportFormat, ExportMode}, ticket::BlobTicket, util::SetTagOption
 };
-use n0_snafu::{Result, ResultExt};
-use n0_watcher::Watcher as _;
-
-/// Each protocol is identified by its ALPN string.
-///
-/// The ALPN, or application-layer protocol negotiation, is exchanged in the connection handshake,
-/// and the connection is aborted unless both nodes pass the same bytestring.
-const ALPN: &[u8] = b"iroh-example/echo/0";
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let router = start_accept_side().await?;
-    let node_addr = router.endpoint().node_addr().initialized().await?;
-
-    connect_side(node_addr).await?;
-
-    // This makes sure the endpoint in the router is closed properly and connections close gracefully
-    router.shutdown().await.e()?;
-
-    Ok(())
-}
-
-async fn connect_side(addr: NodeAddr) -> Result<()> {
+async fn main() -> anyhow::Result<()> {
+    // Create an endpoint, it allows creating and accepting
+    // connections in the iroh p2p world
     let endpoint = Endpoint::builder().discovery_n0().bind().await?;
 
-    // Open a connection to the accepting node
-    let conn = endpoint.connect(addr, ALPN).await?;
+    // We initialize the Blobs protocol in-memory
+    let blobs = Blobs::memory().build(&endpoint);
 
-    // Open a bidirectional QUIC stream
-    let (mut send, mut recv) = conn.open_bi().await.e()?;
+    // Now we build a router that accepts blobs connections & routes them
+    // to the blobs protocol.
+    let router = Router::builder(endpoint)
+        .accept(iroh_blobs::ALPN, blobs.clone())
+        .spawn();
 
-    // Send some data to be echoed
-    send.write_all(b"za warudo").await.e()?;
+    // We use a blobs client to interact with the blobs protocol we're running locally:
+    let blobs_client = blobs.client();
 
-    // Signal the end of data for this particular stream
-    send.finish().e()?;
+    // Grab all passed in arguments, the first one is the binary itself, so we skip it.
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    // Convert to &str, so we can pattern-match easily:
+    let arg_refs: Vec<&str> = args.iter().map(String::as_str).collect();
 
-    // Receive the echo, but limit reading up to maximum 1000 bytes
-    let response = recv.read_to_end(1000).await.e()?;
-    assert_eq!(&response, b"za warudo");
+    match arg_refs.as_slice() {
+        ["send", filename] => {
+            let filename: PathBuf = filename.parse()?;
+            let abs_path = std::path::absolute(&filename)?;
 
-    println!("got response: {}", String::from_utf8(response).unwrap());
+            println!("Hashing file.");
 
-    // Explicitly close the whole connection.
-    conn.close(0u32.into(), b"bye!");
+            // keep the file in place and link it, instead of copying it into the in-memory blobs database
+            let in_place = true;
+            let blob = blobs_client
+                .add_from_path(abs_path, in_place, SetTagOption::Auto, WrapOption::NoWrap)
+                .await?
+                .finish()
+                .await?;
 
-    // The above call only queues a close message to be sent (see how it's not async!).
-    // We need to actually call this to make sure this message is sent out.
-    endpoint.close().await;
-    // If we don't call this, but continue using the endpoint, we then the queued
-    // close call will eventually be picked up and sent.
-    // But always try to wait for endpoint.close().await to go through before dropping
-    // the endpoint to ensure any queued messages are sent through and connections are
-    // closed gracefully.
-    Ok(())
-}
+            let node_id = router.endpoint().node_id();
+            let ticket = BlobTicket::new(node_id.into(), blob.hash, blob.format)?;
 
-async fn start_accept_side() -> Result<Router> {
-    let endpoint = Endpoint::builder().discovery_n0().bind().await?;
+            println!("File hashed. Fetch this file by running:");
+            println!("cargo run --example transfer -- receive {ticket} path");
 
-    // Build our protocol handler and add our protocol, identified by its ALPN, and spawn the node.
-    let router = Router::builder(endpoint).accept(ALPN, Echo).spawn();
+            tokio::signal::ctrl_c().await?;
+        }
+        ["receive", ticket, filename] => {
+            let filename: PathBuf = filename.parse()?;
+            let abs_path = std::path::absolute(filename)?;
+            let ticket: BlobTicket = ticket.parse()?;
 
-    Ok(router)
-}
+            println!("Starting download.");
 
-#[derive(Debug, Clone)]
-struct Echo;
+            blobs_client
+                .download(ticket.hash(), ticket.node_addr().clone())
+                .await?
+                .finish()
+                .await?;
 
-impl ProtocolHandler for Echo {
-    /// The `accept` method is called for each incoming connection for our ALPN.
-    ///
-    /// The returned future runs on a newly spawned tokio task, so it can run as long as
-    /// the connection lasts.
-    async fn accept(&self, connection: Connection) -> Result<(), AcceptError> {
-        // We can get the remote's node id from the connection.
-        let node_id = connection.remote_node_id()?;
-        println!("accepted connection from {node_id}");
+            println!("Finished download.");
 
-        // Our protocol is a simple request-response protocol, so we expect the
-        // connecting peer to open a single bi-directional stream.
-        let (mut send, mut recv) = connection.accept_bi().await?;
+            println!("Copying to destination.");
 
-        // Echo any bytes received back directly.
-        // This will keep copying until the sender signals the end of data on the stream.
-        let bytes_sent = tokio::io::copy(&mut recv, &mut send).await?;
-        println!("Copied over {bytes_sent} byte(s)");
+            blobs_client
+                .export(
+                    ticket.hash(),
+                    abs_path,
+                    ExportFormat::Blob,
+                    ExportMode::Copy,
+                )
+                .await?
+                .finish()
+                .await?;
 
-        // By calling `finish` on the send stream we signal that we will not send anything
-        // further, which makes the receive stream on the other end terminate.
-        send.finish()?;
-
-        // Wait until the remote closes the connection, which it does once it
-        // received the response.
-        connection.closed().await;
-
-        Ok(())
+            println!("Finished copying.");
+        }
+        _ => {
+            println!("Couldn't parse command line arguments: {args:?}");
+            println!("Usage:");
+            println!("    # to send:");
+            println!("    cargo run --example transfer -- send [FILE]");
+            println!("    # this will print a ticket.");
+            println!();
+            println!("    # to receive:");
+            println!("    cargo run --example transfer -- receive [TICKET] [FILE]");
+        }
     }
+
+    // Gracefully shut down the router
+    println!("Shutting down.");
+    router.shutdown().await?;
+
+    Ok(())
 }
